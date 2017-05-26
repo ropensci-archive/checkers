e2f167205dc4474d213d126e5f574943ab176aea
Laura DeCicco <ldecicco@usgs.gov>
2017-05-26 14:18:05 -0500

    Add a test

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -3,6 +3,10 @@ context("checkers")
 
 test_that("checkers works", {
 
-  expect_true(TRUE)
+  check_results <- gp_check(path=system.file("scripts", package="checkers"),
+           checks = "comments",
+           extra_preps = list(scripts = prep_scripts),
+           extra_checks = list(comments = check_well_commented))
+  expect_type(check_results, "list")
 
 })
