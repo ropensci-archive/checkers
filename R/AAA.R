792df8a2503a56847c0c3b9faceb850f3d52d96f
Laura DeCicco <ldecicco@usgs.gov>
2017-05-26 12:44:25 -0500

    Adding config file

---
+++ R/AAA.R
@@ -0,0 +1,26 @@
+checker_config <- new.env(parent = emptyenv())
+
+
+#' @title Load checkers config
+#'
+#' @description Loads the config file into options which are
+#'used elsewhere in the application.
+#'
+#' @param filename string to custom file
+#'
+#' @examples
+#' load_config()
+#' @export
+#' @importFrom yaml yaml.load_file
+load_config = function(filename) {
+
+  if(missing(filename)){
+    filename <- system.file("extdata", "default.yaml", package = "checkers")
+  }
+
+  checkers <- yaml.load_file(filename)
+
+  checker_config$options <- checkers
+}
+
+
