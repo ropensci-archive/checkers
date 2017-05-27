d1fdf6a1a6dcb26ccb01b48f62f875aaa8aa85de
Laura DeCicco <ldecicco@usgs.gov>
2017-05-26 20:17:21 -0500

    if this than that

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -29,11 +29,18 @@ test_that("checkers works", {
   expect_type(check_results, "list")
 
   check_xml <- make_pref_pkg_check("XML", "xml2")
-  gp_check(path=system.file("scripts", package="checkers"),
+  pref_package <- gp_check(path=system.file("scripts", package="checkers"),
            checks="XML",
            extra_preps=list(packages=prep_packages),
            extra_checks = list(XML = check_xml))
+  expect_type(pref_package, "list")
 
+  check_gam <- make_fun_pair_check("gam", "gam.check")
+  check_funs <- gp_check(path=system.file("scripts", package="checkers"),
+           checks = "check_gam",
+    extra_preps = list(functions = prep_functions),
+    extra_checks = list(check_gam = check_gam))
+  expect_type(check_funs, "list")
 })
 
 
